abfb523d77221846e98ea815a6697641e3079fde
Jonathan Tiret <jtiret@hipay.com>
2015-10-09 18:23:12 +0200

    Adds order mapper class and test file

---
+++ Example/Tests/HiPayTPP/Core/Gateway/Mapper/HPTOrderMapperTests.m
@@ -0,0 +1,38 @@
+//
+//  HPTOrderMapperTests.m
+//  HiPayTPP
+//
+//  Created by Jonathan TIRET on 09/10/2015.
+//  Copyright © 2015 Jonathan TIRET. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <HiPayTPP/HPTAbstractMapper+Decode.h>
+
+@interface HPTOrderMapperTests : XCTestCase
+
+@end
+
+@implementation HPTOrderMapperTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testInitWithWrongData
+{
+    NSDictionary *rawData = @{
+                              @"wrongData": @"anything",
+                              };
+    
+    XCTAssertNil([[HPTOrderMapper alloc] initWithRawData:rawData]);
+    
+}
+
+@end
---
+++ HiPayTPP/Core/Gateway/Mapper/HPTOrderMapper.m
@@ -0,0 +1,13 @@
+//
+//  HPTOrderMapper.m
+//  Pods
+//
+//  Created by Jonathan TIRET on 09/10/2015.
+//
+//
+
+#import "HPTOrderMapper.h"
+
+@implementation HPTOrderMapper
+
+@end

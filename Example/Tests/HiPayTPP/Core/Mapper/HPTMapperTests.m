478deb513129979ac88f2efd48dea14d1f3ddf71
Jonathan Tiret <jtiret@hipay.com>
2015-10-09 20:56:02 +0200

    Adds abstract mapper tests

--- Example/Tests/HiPayTPP/Core/Mapper/HPTMapperTests.m
+++ Example/Tests/HiPayTPP/Core/Mapper/HPTMapperTests.m
@@ -94,10 +94,115 @@
 
     // Chars
     XCTAssertEqualObjects([mapper getEnumCharForKey:@"charValue"], @('U'));
-    XCTAssertEqualObjects([mapper getEnumCharForKey:@"wrongCharValue"], @(-1));
-    XCTAssertEqualObjects([mapper getEnumCharForKey:@"number"], @(-1));
-    XCTAssertEqualObjects([mapper getEnumCharForKey:@"test"], @(-1));
-    XCTAssertEqualObjects([mapper getEnumCharForKey:@"null"], @(-1));
+    XCTAssertEqualObjects([mapper getEnumCharForKey:@"wrongCharValue"], @(' '));
+    XCTAssertEqualObjects([mapper getEnumCharForKey:@"number"], @(' '));
+    XCTAssertEqualObjects([mapper getEnumCharForKey:@"test"], @(' '));
+    XCTAssertEqualObjects([mapper getEnumCharForKey:@"null"], @(' '));
+}
+
+- (void)testLowercaseString
+{
+    NSDictionary *rawData = @{@"test": @"HELLO", @"test2": @"hello", @"test3": @"heLLo"};
+    HPTAbstractMapper *mapper = [[HPTAbstractMapper alloc] initWithRawData:rawData];
+    
+    XCTAssertEqualObjects([mapper getLowercaseStringForKey:@"test"], @"hello");
+    XCTAssertEqualObjects([mapper getLowercaseStringForKey:@"test2"], @"hello");
+    XCTAssertEqualObjects([mapper getLowercaseStringForKey:@"test3"], @"hello");
+    
+}
+
+- (void)testNumber
+{
+    NSDictionary *rawData = @{
+                              @"test1": @"11.78",
+                              @"test2": @(11.78),
+                              @"test3": @"118165353.78",
+                              @"test4": @(118165353.78),
+                              @"test5": @"119",
+                              @"test6": @(119),
+                              @"test7": @"118165353.78986",
+                              @"test8": @(118165353.78986),
+                              @"test9": @"10.87hello",
+                              @"test10": @"hello",
+                              @"test11": @"",
+                              };
+    
+    HPTAbstractMapper *mapper = [[HPTAbstractMapper alloc] initWithRawData:rawData];
+    
+    XCTAssertEqualObjects([mapper getNumberForKey:@"test1"], @(11.78));
+    XCTAssertEqualObjects([mapper getNumberForKey:@"test2"], @(11.78));
+    XCTAssertEqualObjects([mapper getNumberForKey:@"test3"], @(118165353.78));
+    XCTAssertEqualObjects([mapper getNumberForKey:@"test4"], @(118165353.78));
+    XCTAssertEqualObjects([mapper getNumberForKey:@"test5"], @(119));
+    XCTAssertEqualObjects([mapper getNumberForKey:@"test6"], @(119));
+    XCTAssertEqualObjects([mapper getNumberForKey:@"test7"], @(118165353.78986));
+    XCTAssertEqualObjects([mapper getNumberForKey:@"test8"], @(118165353.78986));
+    XCTAssertNil([mapper getNumberForKey:@"test9"]);
+    XCTAssertNil([mapper getNumberForKey:@"test10"]);
+    XCTAssertNil([mapper getNumberForKey:@"test11"]);
+    
+}
+
+- (void)testInteger
+{
+    
+    OCMockObject *mapper = [OCMockObject partialMockForObject:[[HPTAbstractMapper alloc] initWithRawData:@{}]];
+    
+    [[[mapper expect] andReturn:@(11.89)] getNumberForKey:@"test1"];
+    [[[mapper expect] andReturn:@(11)] getNumberForKey:@"test2"];
+    [[[mapper expect] andReturn:@(1898981)] getNumberForKey:@"test3"];
+    [[[mapper expect] andReturn:@(1898981.4789765)] getNumberForKey:@"test4"];
+    [[[mapper expect] andReturn:nil] getNumberForKey:@"test5"];
+    [[[mapper expect] andReturn:@(0.89989)] getNumberForKey:@"test6"];
+    [[[mapper expect] andReturn:@(0.19989)] getNumberForKey:@"test7"];
+    [[[mapper expect] andReturn:@(0)] getNumberForKey:@"test8"];
+
+    XCTAssertEqual([((HPTAbstractMapper *) mapper) getIntegerFromKey:@"test1"], 11);
+    XCTAssertEqual([((HPTAbstractMapper *) mapper) getIntegerFromKey:@"test2"], 11);
+    XCTAssertEqual([((HPTAbstractMapper *) mapper) getIntegerFromKey:@"test3"], 1898981);
+    XCTAssertEqual([((HPTAbstractMapper *) mapper) getIntegerFromKey:@"test4"], 1898981);
+    XCTAssertEqual([((HPTAbstractMapper *) mapper) getIntegerFromKey:@"test5"], 0);
+    XCTAssertEqual([((HPTAbstractMapper *) mapper) getIntegerFromKey:@"test6"], 0);
+    XCTAssertEqual([((HPTAbstractMapper *) mapper) getIntegerFromKey:@"test7"], 0);
+    XCTAssertEqual([((HPTAbstractMapper *) mapper) getIntegerFromKey:@"test8"], 0);
+    
+    [mapper verify];
+}
+
+- (void)testEnumValues
+{
+    typedef NS_ENUM(NSInteger, HPTTestEnumValue) {
+        
+        HPTTestEnumValueUnknown,
+        HPTTestEnumValueRed,
+        HPTTestEnumValueWhite,
+        HPTTestEnumValueBlue,
+        
+    };
+    
+    NSDictionary *values = @{@"red": @(HPTTestEnumValueRed),
+                             @"blue": @(HPTTestEnumValueBlue),
+                             @"white": @(HPTTestEnumValueWhite)};
+    
+    OCMockObject *mockedMapper = [OCMockObject partialMockForObject:[[HPTAbstractMapper alloc] initWithRawData:@{}]];
+    HPTAbstractMapper *mapper = (HPTAbstractMapper *)mockedMapper;
+    
+    [[[mockedMapper expect] andReturn:@"white"] getLowercaseStringForKey:@"test1"];
+    [[[mockedMapper expect] andReturn:@"blue"] getLowercaseStringForKey:@"test2"];
+    [[[mockedMapper expect] andReturn:@"red"] getLowercaseStringForKey:@"test3"];
+    [[[mockedMapper expect] andReturn:@"yellow"] getLowercaseStringForKey:@"test4"];
+    [[[mockedMapper expect] andReturn:@""] getLowercaseStringForKey:@"test5"];
+    [[[mockedMapper expect] andReturn:nil] getLowercaseStringForKey:@"test6"];
+    
+    XCTAssertEqual([mapper getIntegerEnumValueWithKey:@"test1" defaultEnumValue:HPTTestEnumValueUnknown allValues:values], HPTTestEnumValueWhite);
+    XCTAssertEqual([mapper getIntegerEnumValueWithKey:@"test2" defaultEnumValue:HPTTestEnumValueUnknown allValues:values], HPTTestEnumValueBlue);
+    XCTAssertEqual([mapper getIntegerEnumValueWithKey:@"test3" defaultEnumValue:HPTTestEnumValueUnknown allValues:values], HPTTestEnumValueRed);
+    XCTAssertEqual([mapper getIntegerEnumValueWithKey:@"test4" defaultEnumValue:HPTTestEnumValueUnknown allValues:values], HPTTestEnumValueUnknown);
+    XCTAssertEqual([mapper getIntegerEnumValueWithKey:@"test5" defaultEnumValue:HPTTestEnumValueUnknown allValues:values], HPTTestEnumValueUnknown);
+    XCTAssertEqual([mapper getIntegerEnumValueWithKey:@"test6" defaultEnumValue:HPTTestEnumValueUnknown allValues:values], HPTTestEnumValueUnknown);
+    
+    [mockedMapper verify];
+    
 }
 
 @end

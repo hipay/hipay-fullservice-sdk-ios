e11fafca316c792275afd5ab56007a037294fb13
Jonathan Tiret <jtiret@hipay.com>
2015-10-13 16:23:01 +0200

    Tests and implements transaction relations mapping

--- Example/Tests/HiPayTPP/Core/Gateway/Mapper/HPTTransactionMapperTests.m
+++ Example/Tests/HiPayTPP/Core/Gateway/Mapper/HPTTransactionMapperTests.m
@@ -8,6 +8,7 @@
 
 #import <XCTest/XCTest.h>
 #import <HiPayTPP/HPTAbstractMapper+Decode.h>
+#import <HiPayTPP/HPTTransactionRelatedItemMapper+Private.h>
 
 @interface HPTTransactionMapperTests : XCTestCase
 
@@ -34,4 +35,77 @@
     XCTAssertNil([[HPTTransactionMapper alloc] initWithRawData:rawData]);
     
 }
+
+- (void)testMapping
+{
+    NSDictionary *rawData = @{
+                              @"transactionReference": @"446780277416",
+                              @"state": @"completed",
+                              };
+    
+    // Class mock
+    OCMockObject *mockedMapper = [OCMockObject partialMockForObject:[[HPTTransactionMapper alloc] initWithRawData:rawData]];
+    HPTTransactionMapper *mapper = (HPTTransactionMapper *)mockedMapper;
+    
+    // 3-D Secure mock
+    NSDictionary *threeDSecurePayload = @{};
+    HPTThreeDSecure *threeDSecure = [[HPTThreeDSecure alloc] init];
+    
+    OCMockObject *threeDSecureMockedMapper = [OCMockObject mockForClass:[HPTThreeDSecureMapper class]];
+    [[[threeDSecureMockedMapper expect] andReturn:threeDSecure] mappedObject];
+    
+    id threeDSecureClassMock = OCMClassMock([HPTThreeDSecureMapper class]);
+    OCMStub([threeDSecureClassMock mapperWithRawData:threeDSecurePayload]).andReturn(threeDSecureMockedMapper);
+    [[[mockedMapper expect] andReturn:threeDSecurePayload] getDictionaryForKey:@"threeDSecure"];
+    
+    // Fraud screening mock
+    NSDictionary *fraudScreeningPayload = @{};
+    HPTFraudScreening *fraudScreening = [[HPTFraudScreening alloc] init];
+    
+    OCMockObject *fraudScreeningMockedMapper = [OCMockObject mockForClass:[HPTFraudScreeningMapper class]];
+    [[[fraudScreeningMockedMapper expect] andReturn:fraudScreening] mappedObject];
+    
+    id fraudScreeningClassMock = OCMClassMock([HPTFraudScreeningMapper class]);
+    OCMStub([fraudScreeningClassMock mapperWithRawData:fraudScreeningPayload]).andReturn(fraudScreeningMockedMapper);
+    [[[mockedMapper expect] andReturn:fraudScreeningPayload] getDictionaryForKey:@"fraudScreening"];
+    
+    // Order mock
+    NSDictionary *orderPayload = @{};
+    HPTOrder *order = [[HPTOrder alloc] init];
+    
+    OCMockObject *orderMockedMapper = [OCMockObject mockForClass:[HPTOrderMapper class]];
+    [[[orderMockedMapper expect] andReturn:order] mappedObject];
+    
+    id orderClassMock = OCMClassMock([HPTOrderMapper class]);
+    OCMStub([orderClassMock mapperWithRawData:orderPayload]).andReturn(orderMockedMapper);
+    [[[mockedMapper expect] andReturn:orderPayload] getDictionaryForKey:@"order"];
+    
+    // Payment method mock
+    NSDictionary *paymentMethodPayload = @{};
+    HPTPaymentCardToken *paymentMethod = [[HPTPaymentCardToken alloc] init];
+    
+    OCMockObject *paymentMethodMockedMapper = [OCMockObject mockForClass:[HPTPaymentCardTokenMapper class]];
+    [[[paymentMethodMockedMapper expect] andReturn:paymentMethod] mappedObject];
+    
+    id paymentMethodClassMock = OCMClassMock([HPTPaymentCardTokenMapper class]);
+    OCMStub([paymentMethodClassMock mapperWithRawData:paymentMethodPayload]).andReturn(paymentMethodMockedMapper);
+    [[[mockedMapper expect] andReturn:paymentMethodPayload] getDictionaryForKey:@"paymentMethod"];
+    
+    // Tests
+    HPTTransaction *object = [[HPTTransaction alloc] init];
+    
+    [[[mockedMapper expect] andReturn:object] mappedObjectWithTransactionRelatedItem:[OCMArg isKindOfClass:[HPTTransaction class]]];
+    
+
+    
+    
+    XCTAssertEqualObjects(mapper.mappedObject, object);
+    XCTAssertEqualObjects(object.threeDSecure, threeDSecure);
+    XCTAssertEqualObjects(object.fraudScreening, fraudScreening);
+    XCTAssertEqualObjects(object.order, order);
+    XCTAssertEqualObjects(object.paymentMethod, paymentMethod);
+    
+    [mockedMapper verify];
+}
+
 @end
--- HiPayTPP/Core/Gateway/Mapper/HPTTransactionMapper.m
+++ HiPayTPP/Core/Gateway/Mapper/HPTTransactionMapper.m
@@ -9,8 +9,28 @@
 #import "HPTTransactionMapper.h"
 #import "HPTAbstractMapper+Decode.h"
 #import "HPTTransaction.h"
+#import "HPTTransactionRelatedItemMapper+Private.h"
+#import "HPTThreeDSecureMapper.h"
+#import "HPTFraudScreeningMapper.h"
+#import "HPTOrderMapper.h"
+#import "HPTPaymentCardTokenMapper.h"
 
 @implementation HPTTransactionMapper
+
+- (id _Nonnull)mappedObject
+{
+    HPTTransaction *object = [self mappedObjectWithTransactionRelatedItem:[[HPTTransaction alloc] init]];
+
+    [object setValue:[[HPTThreeDSecureMapper mapperWithRawData:[self getDictionaryForKey:@"threeDSecure"]] mappedObject] forKey:@"threeDSecure"];
+    
+    [object setValue:[[HPTFraudScreeningMapper mapperWithRawData:[self getDictionaryForKey:@"fraudScreening"]] mappedObject] forKey:@"fraudScreening"];
+    
+    [object setValue:[[HPTOrderMapper mapperWithRawData:[self getDictionaryForKey:@"order"]] mappedObject] forKey:@"order"];
+    
+    [object setValue:[[HPTPaymentCardTokenMapper mapperWithRawData:[self getDictionaryForKey:@"paymentMethod"]] mappedObject] forKey:@"paymentMethod"];
+    
+    return object;
+}
 
 - (BOOL)isValid
 {

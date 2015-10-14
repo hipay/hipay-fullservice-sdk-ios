389f70e074e6ec67896053380d8dcd829d916603
Jonathan Tiret <jtiret@hipay.com>
2015-10-14 10:59:14 +0200

    Crates abstract serialization mapping class

---
+++ HiPayTPP/Core/Serialization Mapper/HPTAbstractSerializationMapper+Encode.h
@@ -0,0 +1,13 @@
+//
+//  HPTAbstractSerializationMapper+Encode.h
+//  Pods
+//
+//  Created by Jonathan TIRET on 14/10/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HPTAbstractSerializationMapper (Encode)
+
+@end
---
+++ HiPayTPP/Core/Serialization Mapper/HPTAbstractSerializationMapper.m
@@ -0,0 +1,13 @@
+//
+//  HPTAbstractSerializationMapper.m
+//  Pods
+//
+//  Created by Jonathan TIRET on 14/10/2015.
+//
+//
+
+#import "HPTAbstractSerializationMapper.h"
+
+@implementation HPTAbstractSerializationMapper
+
+@end

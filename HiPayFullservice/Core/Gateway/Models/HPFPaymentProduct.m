a49224c1ca53e0cc04d36c3c4b8a8e2862d8dc4f
Jonathan Tiret <jtiret@hipay.com>
2015-12-09 19:10:22 +0100

    Fixes dependency issues

--- HiPayFullservice/Core/Gateway/Models/HPFPaymentProduct.m
+++ HiPayFullservice/Core/Gateway/Models/HPFPaymentProduct.m
@@ -115,7 +115,7 @@ NSString * _Nonnull const HPFPaymentProductCategoryCodeEWallet = @"ewallet";
     self = [super init];
     if (self) {
         _groupedPaymentProductCodes = paymentProducts;
-        _paymentProductDescription = HPFLocalizedString(@"PAYMENT_PRODUCT_GROUP_PAYMENT_CARD");
+        _paymentProductDescription = NSLocalizedStringFromTableInBundle(@"PAYMENT_PRODUCT_GROUP_PAYMENT_CARD", @"Core", [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"HPFCoreLocalization" ofType:@"bundle"]], nil);
         _paymentProductCategoryCode = HPFPaymentProductCategoryCodeCreditCard;
     }
     return self;

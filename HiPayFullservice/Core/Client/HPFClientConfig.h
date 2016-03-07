54d63413583cc751b08a36fd20da162ab5413c4c
Jonathan Tiret <jtiret@hipay.com>
2016-03-07 16:10:06 +0100

    Adds appledoc documentation

--- HiPayFullservice/Core/Client/HPFClientConfig.h
+++ HiPayFullservice/Core/Client/HPFClientConfig.h
@@ -10,19 +10,53 @@
 
 #define HPFClientConfigCallbackURLHost @"hipay-fullservice"
 
+/**
+ *  This enum lists the different HiPay Fullservice environements you can rely on when using this SDK.
+ */
 typedef NS_ENUM(NSInteger, HPFEnvironment) {
-    
+
+    /**
+     *  The Stage environement, when you are testing your integration.
+     */
     HPFEnvironmentStage,
-    HPFEnvironmentProduction,
     
+    /**
+     *  The Production environement, for when you have finished testing and want your application to go live.
+     */
+    HPFEnvironmentProduction,
 };
 
+/**
+ *  Describes the general configuration to be used be the SDK (username, password, environement, etc.).
+ *  The different software components of the SDK that need to perform requests against the HiPay Fullservice platform will rely on this class to get a proper configruation.
+ */
 @interface HPFClientConfig : NSObject
 
+/**
+ *  The environement to be used.
+ */
 @property (nonatomic, readonly) HPFEnvironment environment;
+
+/**
+ * The API username.
+ * Your API credentials can be found in the Merchant Interface in the Integration section.
+ */
 @property (nonatomic, readonly, nullable) NSString *username;
+
+/**
+ * The API password.
+ * Your API credentials can be found in the Merchant Interface in the Integration section.
+ */
 @property (nonatomic, readonly, nullable) NSString *password;
+
+/**
+ *  The user agent that will be sent alongside HTTP requests.
+ */
 @property (nonatomic, readonly, nullable) NSString *userAgent;
+
+/**
+ *  The full app redirection URL (generated automatically).
+ */
 @property (nonatomic, readonly, nullable) NSURL *appRedirectionURL;
 
 + (instancetype _Nonnull)sharedClientConfig;
--- HiPayFullservice/Core/Gateway/Request/Payment Method/HPFIDealPaymentMethodRequest.h
+++ HiPayFullservice/Core/Gateway/Request/Payment Method/HPFIDealPaymentMethodRequest.h
@@ -8,8 +8,16 @@
 
 #import "HPFAbstractPaymentMethodRequest.h"
 
+/**
+ *  Describes iDeal information to be sent alongside an order request.
+ */
 @interface HPFIDealPaymentMethodRequest : HPFAbstractPaymentMethodRequest
 
+/**
+ *  The issuer bank ID. Check out issuerBanks for more information.
+ *
+ *  @see issuerBanks
+ */
 @property (nonatomic, copy) NSString *issuerBankId;
 
 + (instancetype)iDealPaymentMethodRequestWithIssuerBankId:(NSString *)issuerBankId;

5c42d971455bb608f7c70451eceb5bbde353e3ea
Nicolas FILLION <nfillion21@gmail.com>
2017-10-19 15:48:17 +0200

    Create store view controller

---
+++ HiPayFullservice/Payment Screen/Controller/HPFStoreCardViewController.h
@@ -0,0 +1,12 @@
+//
+//  HPFStoreCardViewController.h
+//  HiPayFullservice
+//
+//  Created by Nicolas FILLION on 18/10/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPFStoreCardViewController : UIViewController
+
+@end
---
+++ HiPayFullservice/Payment Screen/Controller/HPFStoreCardViewController.m
@@ -0,0 +1,36 @@
+//
+//  HPFStoreCardViewController.m
+//  HiPayFullservice
+//
+//  Created by Nicolas FILLION on 18/10/2017.
+//
+
+#import "HPFStoreCardViewController.h"
+
+@interface HPFStoreCardViewController ()
+
+@end
+
+@implementation HPFStoreCardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

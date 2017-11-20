e7737d14d25e0e002b5a2a85768a695e472fce7b
Nicolas FILLION <nfillion21@gmail.com>
2017-11-20 16:38:52 +0100

    Replace datecs library

---
+++ HiPayFullservice/POS/Datecs/DatecsLibrary.framework/Versions/1/Headers/DatecsLibrary.h
@@ -0,0 +1,14 @@
+//
+//  DatecsLibrary.h
+//  DatecsLibraryTest
+//
+//  Created by Olaf van Zandwijk on 24-05-13.
+//  Copyright (c) 2013 SepaSoft Development. All rights reserved.
+//
+
+#ifndef DatecsLibraryTest_DatecsLibrary_h
+#define DatecsLibraryTest_DatecsLibrary_h
+
+#import "PPadSDK.h"
+
+#endif
